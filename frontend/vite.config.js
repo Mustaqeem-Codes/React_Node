eb64a1b90b5a1b8a6d18f96b947d0a5fd91dd2f1
Mustaqeem <mustaqeem.uet@gmail.com>
2026-07-25 17:53:36 +0500

    2nd commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy : {
-      '/api' : 'http://localhost:3000'
+      '/api' : 'https://react-node-3944.vercel.app/'
     },
   },
   plugins: [react()],
